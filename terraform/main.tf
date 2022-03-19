52e498d4a8c801cd4480f60ddc8b04f0dd7e10ed
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2022-03-19 16:16:58 +0900

    fix: Cloud Run auth

--- terraform/main.tf
+++ terraform/main.tf
@@ -47,19 +47,9 @@ resource "google_cloud_run_service" "app" {
 }
 
 # Allow unauthenticated users to invoke the service
-data "google_iam_policy" "noauth" {
-  binding {
-    role = "roles/run.invoker"
-    members = [
-      "allUsers",
-    ]
-  }
-}
-
-resource "google_cloud_run_service_iam_policy" "noauth" {
-  location = google_cloud_run_service.app.location
-  project  = google_cloud_run_service.app.project
+resource "google_cloud_run_service_iam_member" "run_all_users" {
   service  = google_cloud_run_service.app.name
-
-  policy_data = data.google_iam_policy.noauth.policy_data
+  location = google_cloud_run_service.app.location
+  role     = "roles/run.invoker"
+  member   = "allUsers"
 }
